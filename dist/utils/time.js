e520347fa5984fd9bcbd9eb326a11fc81f53c71a
AMPakdel <omg.mgfiotygf@gmail.com>
2023-10-28 15:55:59 +0330

    update

--- dist/utils/time.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CronTime = void 0;
-var CronTime;
-(function (CronTime) {
-    CronTime["Daily"] = "0 0 * * *";
-})(CronTime = exports.CronTime || (exports.CronTime = {}));
